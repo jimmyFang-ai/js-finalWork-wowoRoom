7ed4e2048bb0cdf668d3834cc20cf189a41ac771
jimmyFang-ai <pi20120413@yahoo.com.tw>
2022-11-23 19:44:11 +0800

    git init

--- javaScript/index-animation.js
+++ javaScript/index-animation.js
@@ -36,23 +36,3 @@ document.addEventListener('DOMContentLoaded', function () {
     // Attach the handler
     ele.addEventListener('mousedown', mouseDownHandler);
 });
-// menu 切換
-let menuOpenBtn = document.querySelector('.menuToggle');
-let linkBtn = document.querySelectorAll('.topBar-menu a');
-let menu = document.querySelector('.topBar-menu');
-menuOpenBtn.addEventListener('click', menuToggle);
-
-linkBtn.forEach((item) => {
-    item.addEventListener('click', closeMenu);
-})
-
-function menuToggle() {
-    if (menu.classList.contains('openMenu')) {
-        menu.classList.remove('openMenu');
-    } else {
-        menu.classList.add('openMenu');
-    }
-}
-function closeMenu() {
-    menu.classList.remove('openMenu');
-}
